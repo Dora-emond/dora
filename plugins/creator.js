303a7a7a2b9b069cffa4858fccba23377ab755b1
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-10-26 15:50:29 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   m.reply('Chat jika penting, harap tidak spam.')
   this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-  this.sendContact(m.chat, '628112958665@s.whatsapp.net', 'Fadli', m)
+  this.sendContact(m.chat, '6289601145546@s.whatsapp.net', 'Dora-emond', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
